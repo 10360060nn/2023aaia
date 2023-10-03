d20c81dac72bc0e59e4e92054a176cc78bd1c009
10360060nn <145418172+10360060nn@users.noreply.github.com>
2023-10-03 12:05:55 +0800

    Add files via upload

--- week04_5.py
+++ week04_5.py
@@ -1,4 +1,10 @@
-for i in range(1,10):
-	for j in range(1,10):
-		print(f'{j}x{i}={i*j:2} ',end='')
-	print()
+class Solution:
+    def numIdenticalPairs(self, nums: List[int]) -> int:
+        N = len(nums)
+
+        good = 0
+        for i in range(N):
+            for j in range(i+1,N):
+                if nums[i] == nums[j]:
+                    good += 1
+        return good
